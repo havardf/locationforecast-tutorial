283db980b730ae847e9e6ec13aca561b0a47c331
Håvard Futsæter <havardf@met.no>
2020-05-27 14:03:29 +0200

    Separated DOM element creation logic from forecast content logic.

---
+++ src/createHTMLTable.js
@@ -0,0 +1,54 @@
+function createTable(place, forecast) {
+    let oldweatherForecast = document.getElementById('weatherForecast');
+    let weatherForecast = document.createElement("div");
+    weatherForecast.setAttribute("id", "weatherForecast");
+
+    let forecastTitle = document.createElement('h2');
+    forecastTitle.appendChild(document.createTextNode(place));
+    weatherForecast.appendChild(forecastTitle);
+
+    let weatherTable = document.createElement("table");
+    let tableHeader = document.createElement('thead');
+    tableHeader.appendChild(tableHeaders(forecastParameters));
+    weatherTable.appendChild(tableHeader);
+
+    weatherTable.appendChild(tableRows(forecastParameters, forecast));
+    weatherForecast.appendChild(weatherTable);
+    
+    oldweatherForecast.replaceWith(weatherForecast);
+}
+
+function tableHeaders(parameters) {
+    let tableHeaders = document.createElement('tr');
+    let th = document.createElement('th');
+    th.appendChild(document.createTextNode("Time (UTC)"));
+    tableHeaders.appendChild(th);
+
+    parameters.forEach((parameter) => {
+        let th = document.createElement('th');
+        th.appendChild(document.createTextNode(parameter.tableName));
+        tableHeaders.appendChild(th);
+    })
+
+    return tableHeaders
+}
+
+function tableRows(parameters, forecast) {
+    let tableBody = document.createElement('tbody');
+    forecast.properties.timeseries.forEach((time) => {
+        let forecastValues = document.createElement('tr');
+
+        let td = document.createElement('td');
+        td.appendChild(document.createTextNode(formatTime(time.time)));
+        forecastValues.appendChild(td);
+
+        parameters.forEach((parameter) => {
+            let td = document.createElement('td');
+            td.appendChild(document.createTextNode(parameter.valueFunction(time)));
+            forecastValues.appendChild(td);
+        });
+        tableBody.appendChild(forecastValues)
+    });
+
+    return tableBody;
+}
--- src/weatherforecast.js
+++ src/weatherforecast.js
@@ -36,66 +36,31 @@ function weatherForecast(place) {
         if (this.readyState == 4 && this.status == 203) {
             let forecast = JSON.parse(this.responseText);
 
-            weatherTable(places[place].name, forecast);
+            createTable(places[place].name, forecast);
         }
     };
     xmlhttp.open("GET", places[place].forecastURL, true);
     xmlhttp.send();
 }
 
-function weatherTable(place, forecast) {
-    let oldweatherForecast = document.getElementById('weatherForecast');
-    let weatherForecast = document.createElement("div");
-    weatherForecast.setAttribute("id", "weatherForecast");
-
-    let forecastTitle = document.createElement('h2');
-    forecastTitle.appendChild(document.createTextNode(place));
-    weatherForecast.appendChild(forecastTitle);
-
-    let weatherTable = document.createElement("table");
-    let tableHeader = document.createElement('thead');
-    tableHeader.appendChild(weatherTableHeaders(forecastParameters));
-    weatherTable.appendChild(tableHeader);
-
-    weatherTable.appendChild(weatherTableRows(forecastParameters, forecast));
-    weatherForecast.appendChild(weatherTable);
-    
-    oldweatherForecast.replaceWith(weatherForecast);
+function airTemperature(forecastTime) {
+    return forecastTime.data.instant.details.air_temperature;
 }
 
-function weatherTableHeaders(parameters) {
-    let tableHeaders = document.createElement('tr');
-    let th = document.createElement('th');
-    th.appendChild(document.createTextNode("Time (UTC)"));
-    tableHeaders.appendChild(th);
-
-    parameters.forEach((parameter) => {
-        let th = document.createElement('th');
-        th.appendChild(document.createTextNode(parameter.tableName));
-        tableHeaders.appendChild(th);
-    })
-
-    return tableHeaders
+function precipitation(forecastTime) {
+    if (forecastTime.data.next_1_hours != undefined) {
+        return forecastTime.data.next_1_hours.details.precipitation_amount;
+    }
+    else if (forecastTime.data.next_6_hours != undefined) {
+        return forecastTime.data.next_6_hours.details.precipitation_amount;
+    }
+    else {
+        return "";
+    }
 }
 
-function weatherTableRows(parameters, forecast) {
-    let tableBody = document.createElement('tbody');
-    forecast.properties.timeseries.forEach((time) => {
-        let forecastValues = document.createElement('tr');
-
-        let td = document.createElement('td');
-        td.appendChild(document.createTextNode(formatTime(time.time)));
-        forecastValues.appendChild(td);
-
-        parameters.forEach((parameter) => {
-            let td = document.createElement('td');
-            td.appendChild(document.createTextNode(parameter.valueFunction(time)));
-            forecastValues.appendChild(td);
-        });
-        tableBody.appendChild(forecastValues)
-    });
-
-    return tableBody;
+function windSpeed(forecastTime) {
+    return forecastTime.data.instant.details.wind_speed;
 }
 
 function formatTime(timestamp) {
@@ -110,33 +75,4 @@ function formatTime(timestamp) {
         time.getDate() + " " + monthNames[time.getMonth()] + ",  " + hourOfDay;
 
     return formattedTime;
-}
-
-function airTemperature(forecastTime) {
-    return forecastTime.data.instant.details.air_temperature;
-}
-
-function precipitation(forecastTime) {
-    if (hasProperty(forecastTime, 'data.next_1_hours.details.precipitation_amount')) {
-        return forecastTime.data.next_1_hours.details.precipitation_amount;
-    }
-    else if (hasProperty(forecastTime, 'data.next_6_hours.details.precipitation_amount')) {
-        return forecastTime.data.next_6_hours.details.precipitation_amount;
-    }
-    else {
-        return "";
-    }
-}
-
-function windSpeed(forecastTime) {
-    return forecastTime.data.instant.details.wind_speed;
-}
-
-function hasProperty(obj, key) {
-    return key.split(".").every(function(x) {
-        if(typeof obj != "object" || obj === null || ! x in obj)
-            return false;
-        obj = obj[x];
-        return true;
-    });
 }
